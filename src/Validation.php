09b8202103678ae18db4e4f11836a45ad50a9600
arman taghiyan <taghiyan98@gmail.com>
2022-03-24 12:02:46 +0430

    change require method to has method

--- src/Validation.php
+++ src/Validation.php
@@ -4,7 +4,7 @@ namespace ArmanValidation;
 
 class Validation
 {
-    public function required(array $array, string $filed): bool
+    public function has(array $array, string $filed): bool
     {
         if (isset($array[$filed])) {
             return true;
